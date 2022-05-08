60c95dcc3e0eeaf0e9a078d86c789fcf17445fbb
Pranalijadhav03 <jadhav.pranali2403@gmail.com>
2022-05-08 21:19:21 +0530

    Find Maximum number and demo problems

---
+++ Genric/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Genric;
+
+
+int[] intArray = { 1, 2, 3, 4, 5 };
+double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+//Program.toPrint(intArray);
+//Program.toPrint(doubleArray);
+//Program.toPrint(charArray);
+
+sample.toPrint<int>(intArray);
+sample.toPrint<double>(doubleArray);
+sample.toPrint<char>(charArray);
+Console.ReadKey();
